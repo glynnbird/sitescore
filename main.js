2a78e8da669352a70f4e779cd4289812a137a56c
Glynn Bird <glynn@scoot.co.uk>
2013-09-26 15:45:01 +0100

    mobile,tablet + desktop screenshots

--- main.js
+++ main.js
@@ -43,7 +43,61 @@ app.get('/screenshot', function(req,res) {
       if(exists) {
         res.sendfile(tmpfile);
       } else {
+        options = { screenSize: {width: 1440,height: 900},
+                    userAgent: "Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US)"}
         webshot(req.query.url, tmpfile, function(err) {
+          res.sendfile(tmpfile);
+        });
+      }
+    });
+
+  } else {
+    res.send(404, 'Sorry, we cannot find that!');
+  }
+  
+});
+
+app.get('/mscreenshot', function(req,res) {
+  
+  if(typeof req.query.url != "undefined" && req.query.url.length > 0) {
+    var hash = crypto.createHash('md5').update("mobile"+req.query.url).digest("hex");
+    var tmpfile = '/tmp/'+hash+'.png';    
+    
+    fs.exists(tmpfile, function (exists) {
+      if(exists) {
+        res.sendfile(tmpfile);
+      } else {
+         var options = { screenSize: {width: 320,height: 480},
+                         userAgent: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
+                        };
+           
+        webshot(req.query.url, tmpfile, options, function(err) {
+          res.sendfile(tmpfile);
+        });
+      }
+    });
+
+  } else {
+    res.send(404, 'Sorry, we cannot find that!');
+  }
+  
+});
+
+app.get('/tscreenshot', function(req,res) {
+  
+  if(typeof req.query.url != "undefined" && req.query.url.length > 0) {
+    var hash = crypto.createHash('md5').update("tablet"+req.query.url).digest("hex");
+    var tmpfile = '/tmp/'+hash+'.png';    
+    
+    fs.exists(tmpfile, function (exists) {
+      if(exists) {
+        res.sendfile(tmpfile);
+      } else {
+         var options = { screenSize: {width: 768,height: 1024},
+                         userAgent: "Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10"
+                      };
+           
+        webshot(req.query.url, tmpfile, options, function(err) {
           res.sendfile(tmpfile);
         });
       }
